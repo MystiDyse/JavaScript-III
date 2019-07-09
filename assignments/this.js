b8ad86d9ffe530416083168129a604fc01fba90e
Mysti Dyse <mysti@Mystis-MacBook-Pro.local>
2019-07-09 16:48:19 -0700

    completed the explanation portion of the this.js assignment I explained the four principles of the -this- keword as discussed in lecture yesterday.

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. window binding: if the this keyword is in the global scope then it will call the window.
+* 2. implicit binding: when calling a function whatever is left of the '.this' is where this will call from.
+* 3. New binding: the new keyword creates a new object and this will point to the new object.
+* 4. Explicit Binding: when caling a function you can use .call , .apply , or .bind to point to the specific object you want this to call from.
 *
 * write out a code example of each explanation above
 */
@@ -12,7 +12,13 @@
 // Principle 1
 
 // code example for Window Binding
-
+function Kitty(color) {
+    console.log('what a cute ' + color + ' kitty!');
+    console.log(this);
+  }
+  
+  Kitty(brown);
+  
 // Principle 2
 
 // code example for Implicit Binding
